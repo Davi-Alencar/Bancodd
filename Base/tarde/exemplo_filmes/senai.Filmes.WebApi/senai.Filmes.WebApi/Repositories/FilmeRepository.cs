a04fe0b05c771a13ec0d3098e3c96d3e02b361e9
Davi Alencar <davialencar101@gmail.com>
2020-02-19 15:55:37 -0300

    19/02/2020

---
+++ Base/tarde/exemplo_filmes/senai.Filmes.WebApi/senai.Filmes.WebApi/Repositories/FilmeRepository.cs
@@ -0,0 +1,67 @@
+﻿using senai.Filmes.WebApi.Domains;
+using senai.Filmes.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.Filmes.WebApi.Repositories
+{
+    public class FilmeRepository : IFilmeRepository
+    {
+        private string stringConexao = "Data Source=DEV8\\SQLEXPRESS; initial catalog=Filmes_tarde; user Id=sa; pwd=sa@132";
+
+        public FilmeDomain BuscarPorId(int id)
+        {
+            using (SqlConnection Conexao = new SqlConnection(stringConexao))
+            {
+                string querySelecionarPorId = "SELECT IdFilme, Titulo FROM Filmes WHERE IdFilme = @ID";
+
+                Conexao.Open();
+
+                SqlDataReader Leitor;
+
+                using (SqlCommand Comando = new SqlCommand(querySelecionarPorId, Conexao))
+                {
+                    Comando.Parameters.AddWithValue("@ID", id);
+
+                    Leitor = Comando.ExecuteReader();
+
+                    if(Leitor.Read())
+                    {
+                        FilmeDomain filme = new FilmeDomain()
+                        {
+                            IdFilme = Convert.ToInt32(Leitor["IdFilme"]),
+
+                            Titulo = Leitor["Titulo"].ToString(),
+                            
+                            IdGenero = Convert.ToInt32(Leitor["IdGenero"])
+                        };
+                        return filme;
+                    }
+                    return null;
+
+                }   
+            }
+        }
+
+        public void Cadastrar (FilmeDomain filme)
+        {
+            using (SqlConnection Conexao = new SqlConnection(stringConexao))
+            {
+                string queryInsert = "INSERT INTO Fimes(Titulo) VALUES(@Titulo)";
+
+                SqlCommand Comando = new SqlCommand(queryInsert, Conexao);
+
+                Comando.Parameters.AddWithValue("@Titulo", filme.Titulo);
+
+                Conexao.Open();
+
+                Comando.ExecuteNonQuery();
+
+
+            }
+        }
+    }
+}
